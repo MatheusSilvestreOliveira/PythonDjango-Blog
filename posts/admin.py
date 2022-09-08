19ae6e4331ac7a633ea927a58aa2da55b7aa3cf3
Matheus Silvestre <mathsylvester13@gmail.com>
2022-09-08 12:48:02 -0300

    Importing summernote to posts

--- posts/admin.py
+++ posts/admin.py
@@ -1,11 +1,13 @@
 from django.contrib import admin
 from .models import Post
+from django_summernote.admin import SummernoteModelAdmin
 
 
-class PostAdmin(admin.ModelAdmin):
+class PostAdmin(SummernoteModelAdmin):
     list_display = ('post_title', 'post_author', 'post_date', 'post_category', 'show_post',)
     list_editable = ('show_post',)
     list_display_links = ('post_title',)
+    summernote_fields = ('post_content',)
 
 
 admin.site.register(Post, PostAdmin)
