fe991d64a592cbca460e5ec804fb307b3d35ce14
Matheus Silvestre <mathsylvester13@gmail.com>
2022-09-09 14:03:35 -0300

    Fixed posts order and filter, comments count

--- posts/views.py
+++ posts/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.views.generic.list import ListView
 from django.views.generic.edit import UpdateView
 from .models import Post
+from django.db.models import Q, Count, Case, When
 
 
 class PostIndex(ListView):
@@ -9,6 +10,18 @@ class PostIndex(ListView):
     template_name = 'posts/index.html'
     context_object_name = 'posts'
     paginate_by = 3
+
+    def get_queryset(self):
+        qs = super().get_queryset()
+        qs = qs.order_by('-id').filter(show_post=True)
+        qs = qs.annotate(
+            num_commentaries=Count(
+                Case(
+                    When(comments__comment_show=True, then=1)
+                )
+            )
+        )
+        return qs
 
 
 class PostSearch(PostIndex):
