14aee1377c22aa5493c76bf7a8b63bd9ab4bafa2
Matheus Silvestre <mathsylvester13@gmail.com>
2022-09-27 16:25:10 -0300

    Adding google reCAPTCHA to comments form

--- comments/forms.py
+++ comments/forms.py
@@ -1,18 +1,32 @@
 from django.forms import ModelForm
 from .models import Comments
+import requests
 
 
 class FormComment(ModelForm):
     def clean(self):
-        data = self.cleaned_data
-        name = data.get('comment_name')
-        email = data.get('comment_email')
-        comment = data.get('comment')
+        raw_data = self.data
+        recaptcha_response = raw_data.get('g-recaptcha-response')
+        recaptcha_request = requests.post(
+            'https://www.google.com/recaptcha/api/siteverify',
+            data={
+                'secret': '6Lc61DYiAAAAABOeMLfOpUcSwwO3T44zVlP_t29f',
+                'response': recaptcha_response
+            }
+        )
+        recaptcha_result = recaptcha_request.json()
+        if not recaptcha_result.get('success'):
+            self.add_error('comment', 'reCAPTCHA failed, try again.')
+
+        cleaned_data = self.cleaned_data
+        name = cleaned_data.get('comment_name')
+        email = cleaned_data.get('comment_email')
+        comment = cleaned_data.get('comment')
 
         if len(name) < 4:
             self.add_error('comment_name', 'Name length must be longer than 4 characters.')
 
-        if not comment:
+        if not comment and recaptcha_result.get('success'):
             self.add_error('comment', 'Comment is required to submit.')
 
     class Meta:
